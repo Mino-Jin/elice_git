c18d48f71db9cb9a8b0cfe6f8e96523ffc354aee
Jin MinHo <engleswing@gmail.com>
2018-12-21 13:41:04 +0900

    Initial commit

---
+++ music.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import re
+import urllib.request
+
+from bs4 import BeautifulSoup
+from slackclient import SlackClient
+from flask import Flask, request, make_response, render_template
+
+app = Flask(__name__)
+
+slack_token = "xoxp-506652758256-507380688323-507363497492-46c186f6b301793e17a363d1827d098a"
+slack_client_id = "506652758256.507426597427"
+slack_client_secret = "6d90131af74398728205cb27558533d9"
+slack_verification = "dqw2lYyjTvgh7vgQpZnuvzRW"
+sc = SlackClient(slack_token)
+
+# 크롤링 함수 구현하기
+def _crawl_naver_keywords(text):
+
+    url = re.search(r'(https?://\S+)', text.split('|')[0]).group(0)
+    req = urllib.request.Request(url)
+    
+    sourcecode = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(sourcecode, "html.parser")
+    #여기에 함수를 구현해봅시다.
+    keywords = []
+    titles = []
+    artists = []
+    results = []
+    for i, keyword in enumerate(soup.find_all("div", class_="ranking")):
+        if i < 10:
+            keywords.append(keyword.find("strong").get_text().strip())
+    for i, title in enumerate(soup.find_all("p", class_="title")):
+        if i < 10:
+            titles.append(title.get_text().strip())
+    for i, artist in enumerate(soup.find_all("p", class_="artist")):
+        if i < 10:
+            artists.append(artist.get_text().strip())
+            
+    print(keywords)
+    for i in range(10):
+        keywords[i]=str(keywords[i])+"위: "+titles[i]+" / "+artists[i]
+    # 한글 지원을 위해 앞에 unicode u를 붙혀준다.
+    return u'\n'.join(keywords)
+
+# 이벤트 핸들하는 함수
+def _event_handler(event_type, slack_event):
+    print(slack_event["event"])
+
+    if event_type == "app_mention":
+        channel = slack_event["event"]["channel"]
+        text = slack_event["event"]["text"]
+
+        keywords = _crawl_naver_keywords(text)
+        sc.api_call(
+            "chat.postMessage",
+            channel=channel,
+            text=keywords
+        )
+
+        return make_response("App mention message has been sent", 200,)
+
+    # ============= Event Type Not Found! ============= #
+    # If the event_type does not have a handler
+    message = "You have not added an event handler for the %s" % event_type
+    # Return a helpful error message
+    return make_response(message, 200, {"X-Slack-No-Retry": 1})
+
+@app.route("/listening", methods=["GET", "POST"])
+def hears():
+    slack_event = json.loads(request.data)
+
+    if "challenge" in slack_event:
+        return make_response(slack_event["challenge"], 200, {"content_type":
+                                                             "application/json"
+                                                            })
+
+    if slack_verification != slack_event.get("token"):
+        message = "Invalid Slack verification token: %s" % (slack_event["token"])
+        make_response(message, 403, {"X-Slack-No-Retry": 1})
+    
+    if "event" in slack_event:
+        event_type = slack_event["event"]["type"]
+        return _event_handler(event_type, slack_event)
+
+    # If our bot hears things that are not events we've subscribed to,
+    # send a quirky but helpful error response
+    return make_response("[NO EVENT IN SLACK REQUEST] These are not the droids\
+                         you're looking for.", 404, {"X-Slack-No-Retry": 1})
+
+@app.route("/", methods=["GET"])
+def index():
+    return "<h1>Server is ready.</h1>"
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=8080)
